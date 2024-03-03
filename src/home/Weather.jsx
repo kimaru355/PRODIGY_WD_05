ad848eef022c9e7146429a094c6026030be6eafe
kimaru355 <emmanuelkimaru355@gmail.com>
2024-03-03 23:19:16 +0300

    complete weather app with suggestions

--- src/home/Weather.jsx
+++ src/home/Weather.jsx
@@ -39,6 +39,45 @@ function Weather() {
     setSearchInput(event.target.value);
   };
 
+  async function fetchCities() {
+    if (searchInput.length > 2) {
+      try {
+        const url = `https://api.weatherapi.com/v1/search.json?key=d3ec53b7238e4be58fe224501240203&q=${searchInput}`;
+        const response = await fetch(url);
+        if (response.status === 200) {
+          let cities = await response.json();
+          if (cities.length > 0) {
+            notifyRef.current.classList.remove("hidden");
+            notifyRef.current.classList.add("text-black", "flex", "flex-col");
+            while (notifyRef.current.firstChild) {
+              notifyRef.current.removeChild(notifyRef.current.firstChild);
+            }
+            cities.forEach((city) => {
+              let one = document.createElement("button");
+              one.classList.add(
+                "bg-gray-300",
+                "hover:bg-gray-400",
+                "w-full",
+                "rounded-lg",
+                "border",
+                "border-gray-800"
+              );
+              one.textContent = city.name;
+              one.addEventListener("click", () => {
+                setSearchInput(city.name);
+                inputRef.current.value = city.name;
+                fetchWeather();
+              });
+              notifyRef.current.appendChild(one);
+            });
+          }
+        }
+      } catch (e) {
+        console.log("Error:", e);
+      }
+    }
+  }
+
   async function fetchWeather() {
     if (searchInput.length > 2) {
       const url = `https://api.weatherapi.com/v1/current.json?key=d3ec53b7238e4be58fe224501240203&q=${searchInput}`;
@@ -58,6 +97,8 @@ function Weather() {
           setWeather(weather);
           notifyRef.current.classList.add("hidden");
           notifyRef.current.textContent = "";
+          inputRef.current.value = "";
+          setSearchInput("");
         } else if (response.status == 400) {
           notifyRef.current.classList.add("text-red-400");
           notifyRef.current.textContent = `${searchInput} does not exist`;
@@ -89,6 +130,7 @@ function Weather() {
             autoComplete="off"
             ref={inputRef}
             onChange={handleInputChange}
+            onKeyUp={fetchCities}
             className="w-full h-full pl-4 outline-none rounded-full bg-gray-200 focus:bg-gray-400 placeholder:text-gray-600 text-slate-800 focus:placeholder:text-white text-xl"
           />
           <MagnifyingGlassIcon
@@ -97,8 +139,8 @@ function Weather() {
             ref={searchRef}
           />
         </div>
-        <p className="text-4xl hidden" ref={notifyRef}></p>
-        <div className="w-5/6 max-w-4xl">
+        <div className="text-4xl w-2/3 max-w-3xl hidden" ref={notifyRef}></div>
+        <div className="w-5/6 min-w-80 max-w-4xl overflow-auto">
           {weather && (
             <div className="flex flex-col items-center justify-center">
               <h2 className="text-2xl pt-2 pb-4">
@@ -106,7 +148,7 @@ function Weather() {
                 <span className="font-bold"> {weather.location.name}, </span>
                 {weather.location.country}
               </h2>
-              <div className="grid grid-cols-2 gap-4 bg-blue-2 w-full p-4 rounded-lg">
+              <div className="grid grid-cols-2 place-items-center gap-4 bg-blue-2 w-full p-4 rounded-lg">
                 <div className="flex flex-col justify-center">
                   <div className="flex">
                     <img
@@ -142,7 +184,7 @@ function Weather() {
                     />
                     <p className="text-lg">Humidity</p>
                   </div>
-                  <p className="text-6xl text-blue-600">
+                  <p className="text-4xl text-blue-600 sm:text-6xl">
                     {weather.current.humidity}
                     <sup>o</sup>
                   </p>
@@ -156,9 +198,9 @@ function Weather() {
                     />
                     <p className="text-lg">Pressure</p>
                   </div>
-                  <p className="text-6xl text-blue-600">
+                  <p className="text-4xl text-blue-600 sm:text-6xl">
                     {weather.current.pressure_mb}
-                    <span className="text-4xl">mb</span>
+                    <span className="text-2xl sm:text-4xl">mb</span>
                   </p>
                 </div>
                 <div>
@@ -168,13 +210,13 @@ function Weather() {
                     <img
                       src="/compass.svg"
                       alt="compass icon"
-                      className="w-6 ml-4"
+                      className="w-6 ml-2 sm:ml-4"
                     />
                     <p className="text-lg">{weather.current.wind_dir}</p>
                   </div>
-                  <p className="text-6xl text-gray-600">
+                  <p className="text-4xl sm:text-6xl text-gray-600">
                     {weather.current.wind_kph}
-                    <span className="text-4xl">km/h</span>
+                    <span className="text-2xl">km/h</span>
                   </p>
                 </div>
                 <div>
@@ -184,13 +226,13 @@ function Weather() {
                     <img
                       src="/compass.svg"
                       alt="compass icon"
-                      className="w-6 ml-4"
+                      className="w-6 ml-2 sm:ml-4"
                     />
                     <p className="text-lg">{weather.current.wind_dir}</p>
                   </div>
-                  <p className="text-6xl text-gray-600">
+                  <p className="text-4xl sm:text-6xl text-gray-600">
                     {weather.current.gust_kph}
-                    <span className="text-4xl">km/h</span>
+                    <span className="text-2xl">km/h</span>
                   </p>
                 </div>
                 <div>
@@ -198,7 +240,7 @@ function Weather() {
                     <img src="/uv.svg" alt="uv icon" className="w-6" />
                     <p className="text-lg">UV</p>
                   </div>
-                  <p className="text-6xl text-yellow-600">
+                  <p className="text-4xl sm:text-6xl text-yellow-600">
                     {weather.current.uv}
                     <span className="text-4xl"> of 11</span>
                   </p>
@@ -208,7 +250,7 @@ function Weather() {
                     <img src="/visibility.svg" alt="uv icon" className="w-6" />
                     <p className="text-lg">Visibility</p>
                   </div>
-                  <p className="text-6xl text-black">
+                  <p className="text-4xl sm:text-6xl text-black">
                     {weather.current.vis_km}
                     <span className="text-4xl">km</span>
                   </p>
